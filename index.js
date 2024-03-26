6b5eee32dd8f52792caba581f9765071554866ad
creflobrian@gmail.com <creflobrian@gmail.com>
2024-03-26 22:12:08 +0300

    complete advanced functions lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    return ['Antonia', 'Nuru'];
+}
+const returnLastTwoDrivers = function() {
+    return ['Amari', 'Mo'];
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const drivers = ['Antonia','Nuru','Amari','Mo'];
+const selectDifferentDrivers = function(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
